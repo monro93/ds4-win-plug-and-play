c2a3f521f1d02c3cf154081f8dfbc5edb8ec1af2
Albert Montolio Romo <albertmonro@gmail.com>
2017-10-12 14:21:11 +0200

    Upload all the old code

---
+++ Mando_Ps4_PC/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Mando_Ps4_PC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbInfo = new System.Windows.Forms.Label();
+            this.btPrincipal = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuConfig = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuUbicacion = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuPreguntarCerrar = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbInfo
+            // 
+            this.lbInfo.AutoSize = true;
+            this.lbInfo.Location = new System.Drawing.Point(42, 57);
+            this.lbInfo.Name = "lbInfo";
+            this.lbInfo.Size = new System.Drawing.Size(88, 13);
+            this.lbInfo.TabIndex = 0;
+            this.lbInfo.Text = "Por favor, espere";
+            // 
+            // btPrincipal
+            // 
+            this.btPrincipal.Location = new System.Drawing.Point(45, 115);
+            this.btPrincipal.Name = "btPrincipal";
+            this.btPrincipal.Size = new System.Drawing.Size(75, 23);
+            this.btPrincipal.TabIndex = 1;
+            this.btPrincipal.Text = "Empezar";
+            this.btPrincipal.UseVisualStyleBackColor = true;
+            this.btPrincipal.Click += new System.EventHandler(this.btPrincipal_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuConfig});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuConfig
+            // 
+            this.menuConfig.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuUbicacion,
+            this.menuPreguntarCerrar});
+            this.menuConfig.Name = "menuConfig";
+            this.menuConfig.Size = new System.Drawing.Size(95, 20);
+            this.menuConfig.Text = "Configuración";
+            // 
+            // menuUbicacion
+            // 
+            this.menuUbicacion.Name = "menuUbicacion";
+            this.menuUbicacion.Size = new System.Drawing.Size(248, 22);
+            this.menuUbicacion.Text = "Ubicación InputMapper";
+            this.menuUbicacion.Click += new System.EventHandler(this.menuUbicacion_Click);
+            // 
+            // menuPreguntarCerrar
+            // 
+            this.menuPreguntarCerrar.CheckOnClick = true;
+            this.menuPreguntarCerrar.Name = "menuPreguntarCerrar";
+            this.menuPreguntarCerrar.Size = new System.Drawing.Size(248, 22);
+            this.menuPreguntarCerrar.Text = "Preguntarme al cerrar programas";
+            this.menuPreguntarCerrar.CheckedChanged += new System.EventHandler(this.menuPreguntarCerrar_CheckedChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "InputMapper | InputMapper.exe, DS4Windows.exe";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btPrincipal);
+            this.Controls.Add(this.lbInfo);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbInfo;
+        private System.Windows.Forms.Button btPrincipal;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuConfig;
+        private System.Windows.Forms.ToolStripMenuItem menuUbicacion;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem menuPreguntarCerrar;
+    }
+}
+
---
+++ Mando_Ps4_PC/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibUsbDotNet;
+using System.Management;
+
+namespace Mando_Ps4_PC
+{
+    public partial class Form1 : Form
+    {
+        private String rutaInputMapper;
+        private List<String> programasListaNegra;
+        private List<String> programasACerrar;
+        private bool primerClick = true;
+
+        public Form1()
+        {
+            
+            programasListaNegra = new List<string>();
+            InitializeComponent();
+            //lecturaSettings();
+            //inicializarListaNegra();
+            mandoConectat();
+        }
+        private void buscarInputMapper()
+        {
+            if (DialogResult.OK == MessageBox.Show("Por favor, localize el archivo InputMapper.exe o DS4Windows.exe en su equipo."))
+            {
+                String directori_inicial = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86),
+                    "DSDCS", "DS4Windows");
+                if (!Directory.Exists(directori_inicial))
+                    directori_inicial = "C:\\";
+
+                openFileDialog1.InitialDirectory = directori_inicial;
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    rutaInputMapper = openFileDialog1.FileName;
+                    Properties.Settings.Default.Ruta_InputMapper = rutaInputMapper;
+                    Properties.Settings.Default.Save();
+                }
+            }
+        }
+
+        private void lecturaSettings()
+        {
+            if (Properties.Settings.Default.Preguntar_Cerrar)
+            {
+                menuPreguntarCerrar.Checked = true;
+            }
+            else
+            {
+                menuPreguntarCerrar.Checked = false;
+            }
+
+            if (Properties.Settings.Default.Ruta_InputMapper != "")
+            {
+                rutaInputMapper = Properties.Settings.Default.Ruta_InputMapper;
+            }
+            else
+            {
+                buscarInputMapper();
+            }
+        }
+
+        private void inicializarListaNegra()
+        {
+            programasListaNegra.Add("explorer");
+            programasListaNegra.Add("Steam");
+            programasListaNegra.Add("ts3client_win64");
+            programasListaNegra.Add("ts3client_win32");
+        }
+
+        private bool cerrarProgramasListaNegra()
+        {
+            
+            StringBuilder str = new StringBuilder();
+            programasACerrar = new List<string>();
+
+            foreach(String program in programasListaNegra)
+            {
+                Process[] instancias = Process.GetProcessesByName(program);
+                Console.WriteLine(program);
+                if (instancias.Length > 0)
+                {
+                    programasACerrar.Add(program);
+                    str.Append(", " + program);
+                }
+                
+                
+            }
+            if (programasACerrar.Count == 0)
+                return true;
+
+            DialogResult mes = DialogResult.None;
+            if (menuPreguntarCerrar.Checked)
+            {
+                mes = MessageBox.Show(String.Format("¿Quiere cerrar los programas {0}?", str.ToString()),"Cerrar Programas Conflictivos", MessageBoxButtons.OKCancel);
+            }
+            if(!menuPreguntarCerrar.Checked || mes == DialogResult.OK)
+            {
+                Console.WriteLine("Programes tancats: ------");
+                foreach (String program in programasACerrar)
+                {
+                    Process[] instancias = Process.GetProcessesByName(program);
+                    foreach(Process p in instancias)
+                    {
+                        try {
+                            Console.WriteLine(p.ProcessName);
+                            ForceKill(p);
+                        }catch(Exception e)
+                        {
+                            Console.WriteLine("programa no tancat");
+                            return false;
+                        }
+                    }
+                }
+            }else if(mes == DialogResult.Cancel)
+            {
+                return false;
+            }
+            return true;
+        }
+        
+        private void obreProgramesTancats()
+        {
+
+            Process.Start(Path.Combine(Environment.GetEnvironmentVariable("windir"), "explorer.exe"));
+
+        }
+
+        private void obreInputMapper()
+        {
+            ProcessStartInfo p = new ProcessStartInfo("notepad.exe");
+            p.WindowStyle = ProcessWindowStyle.Minimized;
+            Process.Start(p);
+        }
+
+        public bool IsUsbDeviceConnected(string pid, string vid)
+        {
+            using (var searcher =
+              new ManagementObjectSearcher(@"Select * From Win32_USBControllerDevice"))
+            {
+                using (var collection = searcher.Get())
+                {
+                    foreach (var device in collection)
+                    {
+                        
+                        var usbDevice = Convert.ToString(device);
+                        Console.WriteLine(usbDevice);
+                        if (usbDevice.Contains(pid) && usbDevice.Contains(vid))
+                        {
+                            Console.WriteLine("DeviceName:"+device.GetPropertyValue("Name"));
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool mandoConectat()
+        {
+            return IsUsbDeviceConnected("028", "045");
+        }
+
+        private void btPrincipal_Click(object sender, EventArgs e)
+        {
+            if (mandoConectat())
+                lbInfo.Text="mando conectat";
+            else
+                lbInfo.Text = "mando NO conectat";
+            
+            /*if (primerClick)
+            {   //fase tancar
+                primerClick = false;
+                btPrincipal.Text = "Cancelar";
+                Boolean cerrados = cerrarProgramasListaNegra();
+                if (!cerrados)
+                {
+                    while (!cerrados && MessageBox.Show("No se pudieron cerrar los porgramas necesarios. Si no es la primera vez que ve este mensaje trate de ejecutar el programa como Administrador\n ¿Reintentar?", "¿Reintentar?", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        cerrados = cerrarProgramasListaNegra();
+                    }
+                }
+                //fase obir InputMapper
+                //obreInputMapper();
+
+                //fase conecta mando
+
+                //fase obrir explorer
+                obreProgramesTancats();
+            }
+            else
+            {
+                //fase Tornar a obrir
+                primerClick = true;
+                btPrincipal.Text = "Empezar";
+                obreProgramesTancats();
+            }*/
+
+
+        }
+
+        private void menuPreguntarCerrar_CheckedChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Preguntar_Cerrar = menuPreguntarCerrar.Checked;
+            Properties.Settings.Default.Save();
+        }
+
+        private void menuUbicacion_Click(object sender, EventArgs e)
+        {
+            buscarInputMapper();
+        }
+
+        public void ForceKill(Process process)
+        {
+            using (Process killer = new Process())
+            {
+                killer.StartInfo.FileName = "taskkill";
+                killer.StartInfo.Arguments = string.Format("/f /PID {0}", process.Id);
+                killer.StartInfo.CreateNoWindow = true;
+                killer.StartInfo.UseShellExecute = false;
+                killer.Start();
+                killer.WaitForExit();
+                if (killer.ExitCode != 0)
+                {
+                    throw new Win32Exception(killer.ExitCode);
+                }
+            }
+        }
+    }
+}
